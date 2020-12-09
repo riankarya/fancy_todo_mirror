5b35f95c61b6756a1305be84be0a0b674933b8b6
riankarya <riankaryamarpaung@gmail.com>
2020-12-09 21:36:39 +0700

    setting port

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT_FANCY_TO_DO || 3000
 const ToDo = require('./routes/route')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
